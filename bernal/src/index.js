edf8bb8df3a06e39796141a7877aaac7e6086f0a
Saúl Belbey <belbeysaul@hotmail.com>
2023-02-22 01:27:51 -0300

    corrección de errores

--- bernal/src/index.js
+++ bernal/src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
-
-
-
-// export AWS_ACCESS_KEY_ID=AKIAQTRAYTGVPZG6SAWF
-//export AWS_SECRET_ACCESS_KEY=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
